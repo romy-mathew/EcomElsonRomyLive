84b8681d4335c5f12dfe9b742d306c87e4e6323c
romy-mathew <romymathew732@gmail.com>
2026-08-11 21:00:46 +0530

    updated database.js because outdated syntax

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,34 @@
+//const mongoose = require("mongoose");
+
+//const connectDatabase = () => {
+
+//	console.log("DB_URI =", process.env.DB_URI);
+
+  //mongoose
+    //.connect(process.env.DB_URI, {
+      //useNewUrlParser: true,
+      //useUnifiedTopology: true,
+      //useCreateIndex: true,
+    //})
+    //.then((data) => {
+      //console.log(`Mongodb connected with server: ${data.connection.host}`);
+    //});
+//};
+
+//module.exports = connectDatabase;
+
+
 const mongoose = require("mongoose");
 
-const connectDatabase = () => {
+const connectDatabase = async () => {
+  try {
+    const data = await mongoose.connect(process.env.DB_URI);
 
-	console.log("DB_URI =", process.env.DB_URI);
-
-  mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    })
-    .then((data) => {
-      console.log(`Mongodb connected with server: ${data.connection.host}`);
-    });
+    console.log(`MongoDB connected: ${data.connection.host}`);
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDatabase;
